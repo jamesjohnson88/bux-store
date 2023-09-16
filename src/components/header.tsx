038d84eb01b177bfa17ecd789d627c50fcf2830e
James Johnson <jamesjohnson88@gmail.com>
2023-09-16 23:45:01 +0100

    WIP

---
+++ src/components/header.tsx
@@ -0,0 +1,27 @@
+﻿function Header() {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <div className="text-2xl font-bold">
+                    <a href="/">Bux</a>
+                </div>
+
+                {/* Right-side items */}
+                <div className="flex space-x-4">
+                    {/* My Account (Placeholder) */}
+                    <div className="hover:text-gray-300">
+                        <a href="/my-account">My Account</a>
+                    </div>
+
+                    {/* Basket (Placeholder) */}
+                    <div className="hover:text-gray-300">
+                        <a href="/basket">Basket</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/navigation-bar.tsx
@@ -0,0 +1,56 @@
+﻿'use client';
+
+import React, { useState } from 'react';
+
+interface NavItem {
+    label: string;
+    links: string[];
+}
+
+interface NavigationBarProps {
+    items: NavItem[];
+}
+
+const NavigationBar: React.FC<NavigationBarProps> = ({ items }) => {
+    const [largeFont, setLargeFont] = useState(false);
+
+    const toggleFontSize = () => {
+        setLargeFont(!largeFont);
+    };
+
+    return (
+        <nav className={`bg-gray-800 text-white p-4`}>
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Left-side Navigation Items */}
+                <div className="flex space-x-4">
+                    { items.map((item, index) => (
+                        <div key={index} className="relative group">
+                            <button className="hover:text-gray-300">
+                                {item.label} &#9662;
+                            </button>
+                            <ul className="hidden absolute left-0 mt-2 space-y-2 bg-gray-800 text-white group-hover:block">
+                                {item.links.map((link, linkIndex) => (
+                                    <li key={linkIndex}>
+                                        <a href={link}>{link}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Right-side Font Size Toggle */}
+                <div className="flex items-center">
+                    <button
+                        className={`${largeFont ? 'text-sm' : 'text-m'} text-gray-300`}
+                        onClick={toggleFontSize}
+                    >
+                        {largeFont ? 'Increase Font Size' : 'Decrease Font Size'}
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavigationBar;
