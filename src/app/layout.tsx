40e648e2d95dd2a4167cdbc515126071df0defd5
James Johnson <jamesjohnson88@gmail.com>
2023-09-30 20:57:08 +0100

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,10 @@ function RootLayout({ children } : { children: React.ReactNode })
 {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
