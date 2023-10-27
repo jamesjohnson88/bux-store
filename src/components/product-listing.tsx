ef9ae7b4631fe407e6f2ab571e4da850844efbd4
jamesjohnson <jamesjohnson88@gmail.com>
2023-10-27 10:01:48 +0100

    WIP checkin

--- src/components/product-listing.tsx
+++ src/components/product-listing.tsx
@@ -1,23 +1,12 @@
 ﻿"use client";
 import React, { useEffect, useState } from 'react';
 import Product from "@/components/product";
+import LockingPopup from "@/components/locking-pop-up";
 import useProductListingStore from "@/stores/product-listing-store";
 
 interface ProductListingProps {
     // @ts-ignore
     products: Product[];
-}
-
-// todo - make it's own component
-function LockingPopup() {
-    return (
-        <div className="bg-black bg-opacity-50 absolute w-full h-full flex items-start justify-center">
-            <div className="bg-white m-40 p-4 rounded-lg text-center text-black relative z-10">
-                <h2 className="text-2xl font-bold mb-2">Whoops!</h2>
-                <p>Looks like somebody is out of browsing tokens... you&apos;re going to need to win more to continue!</p>
-            </div>
-        </div>
-    );
 }
 
 function ProductListing({ products }: ProductListingProps) {
---
+++ src/pages/api/quiz.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+    message: string
+}
+
+export const runtime = "edge";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+   return await fetch('https://beta-trivia.bongobot.io/?type=multiple&difficulty=hard&limit=1');
+}
