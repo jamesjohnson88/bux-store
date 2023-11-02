9e9cf038dc85560952b4654bbdecdcd8eed4852d
jamesjohnson <jamesjohnson88@gmail.com>
2023-11-02 15:24:12 +0000

    Added 'add to basket' functionality + random VAT display

--- src/components/locking-pop-up.tsx
+++ src/components/locking-pop-up.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LockingPopup() {
     return (
-        <div className="bg-black bg-opacity-50 absolute w-full h-full flex items-start justify-center">
+        <div className="bg-black bg-opacity-50 absolute w-full h-full flex items-start justify-center z-10">
             <div className="bg-white max-w-2xl m-40 p-4 rounded-lg text-center text-black relative z-10">
                 <h2 className="text-2xl font-bold mb-2">Whoops!</h2>
                 <p>Looks like somebody is out of BuxCoin... you&apos;re going to need to &apos;mine&apos; some more to continue!</p>
